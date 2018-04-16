3ab371c18e6d4fdcc793fb9305f9ccb7502a5105
fuzzytyrion <matthewrdennis@gmail.com>
2018-04-16 22:10:08 +0100

    login route

--- server/models/user.js
+++ server/models/user.js
@@ -44,8 +44,8 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
-    user.tokens = user.tokens.concat([{access, token}]);
+    var token = jwt.sign({ _id: user._id.toHexString(), access }, 'abc123').toString();
+    user.tokens = user.tokens.concat([{ access, token }]);
 
     return user.save().then(() => {
         return token;
@@ -71,7 +71,27 @@ UserSchema.statics.findByToken = function (token) {
         'tokens.access': 'auth'
     });
 
-}
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+    var User = this;
+
+    return User.findOne({email}).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
 
 UserSchema.pre('save', function (next) {
     var user = this;
@@ -90,4 +110,4 @@ UserSchema.pre('save', function (next) {
 
 var User = mongoose.model('User', UserSchema);
 
-module.exports = {User};
+module.exports = { User };
